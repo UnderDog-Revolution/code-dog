b04aa630eb7325524108b3720fb2c0e5184c6fb0
SeoYeon Jang <syjang1016@korea.ac.kr>
2024-11-22 17:55:54 +0900

    complete

--- main/deploy-commands.js
+++ main/deploy-commands.js
@@ -4,15 +4,13 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
-// Grab all the command folders from the commands directory you created earlier
+// commands 폴더에서 모든 명령어 파일을 읽어들입니다.
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory you created earlier
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
@@ -24,29 +22,76 @@ for (const folder of commandFolders) {
 	}
 }
 
-// Construct and prepare an instance of the REST module
+// REST 인스턴스를 생성하고 토큰을 설정합니다.
 const rest = new REST().setToken(token);
 
-// and deploy your commands!
+// 비동기로 명령어 등록 수행
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
+		// 서버에 명령어 등록
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
-
-            // 전역 배포의 경우
-            // await rest.put(
-            //     Routes.applicationCommands(clientId),
-            //     { body: commands },
-            // );
 		);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })();
+
+
+// const { REST, Routes } = require('discord.js');
+// const { clientId, guildId, token } = require('./config.json');
+// const fs = require('node:fs');
+// const path = require('node:path');
+
+// const commands = [];
+// // Grab all the command folders from the commands directory you created earlier
+// const foldersPath = path.join(__dirname, 'commands');
+// const commandFolders = fs.readdirSync(foldersPath);
+
+// for (const folder of commandFolders) {
+// 	// Grab all the command files from the commands directory you created earlier
+// 	const commandsPath = path.join(foldersPath, folder);
+// 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+// 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+// 	for (const file of commandFiles) {
+// 		const filePath = path.join(commandsPath, file);
+// 		const command = require(filePath);
+// 		if ('data' in command && 'execute' in command) {
+// 			commands.push(command.data.toJSON());
+// 		} else {
+// 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+// 		}
+// 	}
+// }
+
+// // Construct and prepare an instance of the REST module
+// const rest = new REST().setToken(token);
+
+// // and deploy your commands!
+// (async () => {
+// 	try {
+// 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+// 		// The put method is used to fully refresh all commands in the guild with the current set
+// 		const data = await rest.put(
+// 			Routes.applicationGuildCommands(clientId, guildId),
+// 			{ body: commands },
+
+//             // 전역 배포의 경우
+//             // await rest.put(
+//             //     Routes.applicationCommands(clientId),
+//             //     { body: commands },
+//             // );
+// 		);
+
+// 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+// 	} catch (error) {
+// 		// And of course, make sure you catch and log any errors!
+// 		console.error(error);
+// 	}
+// })();
--- main/index.js
+++ main/index.js
@@ -4,17 +4,20 @@ const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,          // 기본 서버 이벤트
+        GatewayIntentBits.GuildMessages,  // 메시지 관련 이벤트
+        GatewayIntentBits.MessageContent, // 메시지 내용 접근
+        GatewayIntentBits.GuildMembers    // 멤버 정보를 가져오기 위한 인텐트
+    ]
 });
 
+// When the client is ready, run this code (only once).
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
 
 // 커맨드 등록 
 client.commands = new Collection();
@@ -23,50 +26,46 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
 }
-
 
 // 명령어 상호작용 수신
 client.on(Events.InteractionCreate, interaction => {
-	console.log(interaction);
+    console.log(interaction);
 });
-
 
 // 명령 실행
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
 });
-
 
 // Log in to Discord with your client's token
 client.login(token);
